cb9f539334d8a80b11cde28ffd7b5d4cf72db4db
Pengeszikra <pengeszikra@gmail.com>
2019-09-27 11:05:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rogmor
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rogmor)
---
+++ index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './style.css';
+
+const App = props => (
+  <pre>
+  {`
+    Rogmor
+
+    Will be soon there is the brand old MMO RPG released
+
+    off courese this is going to be ultimate program
+
+    good question is Can I connect this repot to my git ?  
+  
+  
+  `}
+  </pre>
+)
+render(<App />, document.getElementById('root'));
