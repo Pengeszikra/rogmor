dd120416364a063d71b351b997fc1dc35762daa0
Pengeszikra <pengeszikra@gmail.com>
2019-10-02 08:56:19 +0200

    sprite sheet handle by scss

--- LayoutTest.js
+++ LayoutTest.js
@@ -2,13 +2,16 @@ import React from 'react';
 import divFactory from './divFactory';
 import asset from './asset';
 
-const [ModalWindow, Page] = divFactory('modal-window', 'page');
-
+const [ModalWindow, Page, FaceSprite] = divFactory('modal-window', 'page', 'face-sprite');
 
 export default props => (
   <Page>
+    <FaceSprite data-face={12} />
+    <FaceSprite data-face={22} />
+    <FaceSprite data-face={55} />
     <ModalWindow>
       Simple modal window with couple of content
+      
     </ModalWindow>
   </Page>
 );
