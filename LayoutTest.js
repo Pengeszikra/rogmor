1dd320f64ae64f0abd8e2f1f7c7396cafe2924bc
Pengeszikra <pengeszikra@gmail.com>
2019-10-03 19:17:51 +0200

    item set

--- LayoutTest.js
+++ LayoutTest.js
@@ -2,16 +2,20 @@ import React from 'react';
 import divFactory from './divFactory';
 import asset from './asset';
 
-const [ModalWindow, Page, FaceSprite, FaceGallery] = divFactory('modal-window', 'page', 'face-sprite', 'face-gallery');
+const [ModalWindow, Page, FaceSprite, FaceGallery, ItemSprite] = divFactory
+      ('modal-window', 'page', 'face-sprite', 'face-gallery', 'item-sprite');
 
 export default props => (
   <Page>
-    <FaceGallery>
-      {Array.from({length: 100}, (_, face) => <FaceSprite data-face={face} />)}
-    </FaceGallery>
     <ModalWindow>
-      Simple modal window with couple of content
-      
+      Hero faces ::
+      <FaceGallery>
+        {Array.from({length: 100}, (_, face) => <FaceSprite data-face={face} />)}
+      </FaceGallery>      
+      Item sets ::
+      <FaceGallery>
+        {Array.from({length: 4}, (_, item) => <ItemSprite data-item={item} />)}
+      </FaceGallery>
     </ModalWindow>
   </Page>
 );
