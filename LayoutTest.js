b97adb147ab6854d01e907be4ba9279f516ccba1
Pengeszikra <pengeszikra@gmail.com>
2019-10-03 10:53:51 +0200

    face-sprite fix

--- LayoutTest.js
+++ LayoutTest.js
@@ -2,13 +2,13 @@ import React from 'react';
 import divFactory from './divFactory';
 import asset from './asset';
 
-const [ModalWindow, Page, FaceSprite] = divFactory('modal-window', 'page', 'face-sprite');
+const [ModalWindow, Page, FaceSprite, FaceGallery] = divFactory('modal-window', 'page', 'face-sprite', 'face-gallery');
 
 export default props => (
   <Page>
-    <FaceSprite data-face={12} />
-    <FaceSprite data-face={22} />
-    <FaceSprite data-face={55} />
+    <FaceGallery>
+      {Array.from({length: 100}, (_, face) => <FaceSprite data-face={face} />)}
+    </FaceGallery>
     <ModalWindow>
       Simple modal window with couple of content
       
