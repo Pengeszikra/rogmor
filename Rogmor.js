ee1cdb23f1463339cb882d38786ed382ce116a99
Pengeszikra <pengeszikra@gmail.com>
2019-10-04 13:33:35 +0200

    progress bar

--- Rogmor.js
+++ Rogmor.js
@@ -8,6 +8,8 @@ const [ModalWindow, Page, FaceSprite, FaceGallery, ItemSprite] = divFactory
 const [LoginWindow, FaceWindow, ChatWindow, DarkPanel, InfoPanel] = divFactory
       ('gui gui-loginw', 'gui gui-storyw', 'gui gui-chatWindow', 'gui gui-transPanelDark', 'gui gui-infow');      
 
+const [ProcessBar] = divFactory('button-b');
+
 const heroStyle = face => ({
   backgroundImage : `url(${asset}faces/face${("0000" + face).slice(-4)}.png)`
 });
@@ -25,11 +27,14 @@ export default props => (
         <FaceSprite data-face={32}  />
         <span>Vladimir Kazarow</span>
         <pre>{`          
+          LEVEL: 12
           HP: 320
           ATK: 150
-          DEF: 70
-          SKILL: <i>Avanger
-        `}</pre>
+          DEF: 70          
+          SKILL: Avanger`}
+        <ProcessBar style={{width: 160, marginLeft: 15}} />
+        </pre>
+        
       </FaceWindow>
       <FaceWindow>
         <FaceSprite data-face={45} />
