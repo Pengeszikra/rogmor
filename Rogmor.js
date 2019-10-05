73ae95e2cd4e407e1c5a49f2aa841c5c802c6970
Pengeszikra <pengeszikra@gmail.com>
2019-10-05 22:38:01 +0200

    process bar

--- Rogmor.js
+++ Rogmor.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import divFactory from './divFactory';
 import asset from './asset';
 
@@ -8,39 +8,52 @@ const [ModalWindow, Page, FaceSprite, FaceGallery, ItemSprite] = divFactory
 const [LoginWindow, FaceWindow, ChatWindow, DarkPanel, InfoPanel] = divFactory
       ('gui gui-loginw', 'gui gui-storyw', 'gui gui-chatWindow', 'gui gui-transPanelDark', 'gui gui-infow');      
 
-const [ProcessBar] = divFactory('button-b');
+const [ProcessBarBg, ProcessBarIndicator] = divFactory('button-b', 'process-bar-indicator');
 
 const heroStyle = face => ({
   backgroundImage : `url(${asset}faces/face${("0000" + face).slice(-4)}.png)`
 });
 
-const Fazon = ({face}) => (
-  <Hero style={heroStyle(face)}>
-    <span>fazon</span>
-  </Hero>
+const ProcessBar = ({process = 1, ...props}) => (
+  <ProcessBarBg {...props}>
+    <ProcessBarIndicator style={{width: process * 248}}/>
+  </ProcessBarBg>
 );
 
-export default props => (
-  <Page>
-    <ModalWindow>
-      <FaceWindow>
-        <FaceSprite data-face={32}  />
-        <span>Vladimir Kazarow</span>
-        <pre>{`          
-          LEVEL: 12
-          HP: 320
-          ATK: 150
-          DEF: 70          
-          SKILL: Avanger`}
-        <ProcessBar style={{width: 160, marginLeft: 15}} />
-        </pre>
-        
-      </FaceWindow>
-      <FaceWindow>
-        <FaceSprite data-face={45} />
-        <span>Odett Alborin</span>
-      </FaceWindow>
+export default props => {
+  const [process, setProcess] = useState(0);
 
-    </ModalWindow>
-  </Page>
-);
+  useEffect(() => {
+    const intervall = setIntervall( 
+      () => setProcess(p => p + 0.001),
+      200
+    );
+
+    return () => clearInterval(intervall);
+  }, []);
+
+  return (
+    <Page>
+      <ModalWindow>
+        <FaceWindow>
+          <FaceSprite data-face={32}  />
+          <span>ladimir Kazarow</span>
+          <pre>{`          
+            LEVEL: 12
+            HP: 320
+            ATK: 150
+            DEF: 70          
+            SKILL: Avanger`}
+          <ProcessBar process={process} style={{width: 160, marginLeft: 15}} />
+          </pre>
+          
+        </FaceWindow>
+        <FaceWindow>
+          <FaceSprite data-face={45} />
+          <span>Odett Alborin</span>
+        </FaceWindow>
+
+      </ModalWindow>
+    </Page>
+  );
+}
