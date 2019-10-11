9086ac7863033503052b5f1e4d4b663593986b9b
Pengeszikra <pengeszikra@gmail.com>
2019-10-11 12:36:38 +0200

    .modal-window-inner

--- Rogmor.js
+++ Rogmor.js
@@ -4,17 +4,22 @@ import asset from './asset';
 import HeroCard from './HeroCard';
 
 
-const [ModalWindow, Page] = styler
-      ('modal-window', 'page');
+const [ModalWindow, Page, ModalWindowInner] = styler
+      ('modal-window', 'page', 'modal-window-ModalWindowInner');
 
 export default props => {
   return (
     <Page>
-      <ModalWindow><pre style={{color:'white', fontSize:18}}>{`
-        Lightweight Retro RPG project for React coding.
-        
-        
-      `}</pre></ModalWindow>
+      <ModalWindow><ModalWindowInner>        
+        <pre style={{color:'white', fontSize:18}}>{`
+Lightweight Retro RPG project for React coding.
+
+import React from 'react';
+export default ({name}) => (
+  <NameBox>{name}</NameBox>
+);
+      `}</pre>
+      </ModalWindowInner></ModalWindow>
       <ModalWindow>
         {[33, 78, 45, 14, 19].map(heroId => <HeroCard hero={{heroId}} />)}
       </ModalWindow>
