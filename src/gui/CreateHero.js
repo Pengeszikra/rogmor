ec12291c0e61752356a814d72da2c979196f077f
Peter Vivo <vivo.peter@protonmail.comgit commit -m retrogit pushgit commit -m next>
2022-06-10 22:15:55 +0200

    solve some names

--- src/gui/CreateHero.js
+++ src/gui/CreateHero.js
@@ -8,7 +8,7 @@ import styler from './scss/styler';
 import { BaseOfAdventure, ChatWindow, FaceSprite, InfoPanel, LoginWindow, NoreboMap, RogmorLogo, Button } from './setOfGuiElements';
 import { heroFactory } from './TeamTest';
 
-export default ({troll}) => {
+export default function CreateHero({troll}) {
   const [{
     hero,
   },{
@@ -25,7 +25,7 @@ export default ({troll}) => {
     <p>Welcome young adventurer!</p>
     <p>You are stepp into another dimension, called: Rogmor and ther is a lot of opportunity to fullfill your dreams, at first stepp is choice your character.</p>
     <p>Don't afraid there is no worst choice, and you can learn something different.</p>
-    <p>Rogmor under chaotic statement at moment so prophecy talking about a skillfull hero, who will be restor odrer and peace to this land.</p>
+    <p>Rogmor under chaotic statement at moment so prophecy talkimng about a skillfull hero, who will be restor odrer and peace to this land.</p>
     {/* <section style={{overflowX:'auto'}}><NoreboMap/></section> */}
     <section className="large-button-group" style={{width:200, margin:'0 auto'}}>
       <Button inset="primary" onClick={handleRollHero}>Roll your character</Button>
