36421077ad548ebf3460509dc6029b04bf72f512
Peter Vivo <vivo.peter@protonmail.comgit commit -m retrogit pushgit commit -m next>
2022-07-26 15:30:24 +0200

    multi hero color tw poc

--- src/components/TailwindArea.tsx
+++ src/components/TailwindArea.tsx
@@ -2,9 +2,36 @@ import React from 'react';
 
 export default function TailwindArea ({state, army}) {
 
+  const {hero, focus, entities} = state;
+
   return (
-    <section className="m-4 p-4 rounded-2xl absolute top-0 left-0 overflow-hidden -hidden h-64 bg-stone-900">
-      <pre className="overflow-scroll h-64">{JSON.stringify(state, null, 2)}</pre>
+    <section className="absolute top-0 left-0 overflow-hidden pointer-events-none grid items-center justify-center w-screen">
+      <section className='flex gap-4 m-8'>
+        <section className='grid items-center justify-center w-32 h-32 rounded-3xl bg-orange-700'>
+          <figure className='face-sprite big-face' data-face={hero?.heroId} />
+        </section>
+        <section className='grid items-center justify-center w-32 h-32 rounded-3xl bg-orange-700'>
+          <figure className='face-sprite big-face' data-face={hero?.heroId} />
+        </section>
+        <section className='grid items-center justify-center w-32 h-32 rounded-3xl bg-orange-700'>
+          <figure className='face-sprite big-face' data-face={hero?.heroId} />
+        </section>
+      </section>
+      <div className='h-4 pointer-events-none' />
+      {focus && (
+        <section className='flex gap-4 m-8'>
+          <section className='grid items-center justify-center w-32 h-32 rounded-3xl bg-orange-700'>
+            <figure className='face-sprite big-face' data-face={entities?.[focus]?.heroId} />
+          </section>
+          <section className='grid items-center justify-center w-32 h-32 rounded-3xl bg-orange-700'>
+            <figure className='face-sprite big-face' data-face={entities?.[focus]?.heroId} />
+          </section>
+          <section className='grid items-center justify-center w-32 h-32 rounded-3xl bg-orange-700'>
+            <figure className='face-sprite big-face' data-face={entities?.[focus]?.heroId} />
+          </section>
+        </section>
+      )}
+      <pre className="hidden overflow-scroll h-64 rounded-2xl bg-stone-900">{JSON.stringify(state, null, 2)}</pre>
     </section>
 
   );
