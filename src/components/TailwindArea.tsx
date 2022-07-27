43e977ca7e066e9b41aef617ae7655ead2b1299a
Peter Vivo <vivo.peter@protonmail.comgit commit -m retrogit pushgit commit -m next>
2022-07-27 16:16:11 +0200

    level indicator

--- src/components/TailwindArea.tsx
+++ src/components/TailwindArea.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-export const EntityCard = ({face, tw=""}) => (
+export const EntityCard = ({mob, tw=""}) => (
   <figure className={`flex gap-0 w-32 h-48 rounded-3xl bg-sky-600 justify-center flex-wrap items-center hover:brightness-110 ${tw}`}>
-    <section className='grid items-center justify-center w-24 h-24'>
-      <figure className='face-sprite big-face' data-face={face} />
+    <section className='grid items-center justify-center w-24 h-24 relative'>
+      <figure className='face-sprite big-face' data-face={mob?.heroId} />
+      <div className="absolute rounded-full bg-red-800 p-3 text-lg text-white w-9 h-8 flex items-center justify-center right-24">
+        <span>{mob?.level}</span>
+      </div>
     </section>
     <section className='flex gap-2 items-center justify-center w-max m-4'>
       <div className='bg-rose-800 w-4 h-16'></div>
@@ -13,37 +16,40 @@ export const EntityCard = ({face, tw=""}) => (
   </figure>
 );
 
+export const PlaceOfMob = () => (<div className='w-32' />);
+
 export default function TailwindArea ({state, army}) {
 
   const {hero, focus, entities} = state;
-  const {modHero, setGameState, setupEntities, focusOn, fight, skill, talk, playActionAnim, setHero, levelUpHero} = army;
+  const {modHero, setGameState, setupEntities, focusOn, fight, skill, talk, playActionAnim, setHero, levelUpHero, } = army;
 
 
   return (
-    <section className="absolute top-0 left-0 overflow-hidden --pointer-events-none grid justify-center w-screen items-center">
+    <section className="absolute top-0 left-0 overflow-hidden --pointer-events-none grid justify-center w-screen items-center">      
       {entities && entities[focus] && (
-        <section className='flex gap-4 m-8'>
-          <EntityCard tw="bg-stone-600" face={38} />
-          <EntityCard tw="bg-stone-700" face={entities?.[focus]?.heroId} />
-          <EntityCard tw="bg-stone-600" face={44} />
-        </section>
-      )}
-      {entities && entities[focus] && (
-      <section className="inbox-interaction">
-        <section className="w-96 grid grid-cols-2 gap-2">
-          <button className="rounded-lg p-2 text-lg bg-orange-400" onClick={ _ => {fight()}}>Fight</button>
-          <button className="rounded-lg p-2 text-lg bg-orange-400" onClick={ _ => {skill()}}>Skill</button>
-          <button className="rounded-lg p-2 text-lg bg-orange-400" onClick={ _ => {talk()}}>Talk</button>
-          <button className="rounded-lg p-2 text-lg bg-orange-400" onClick={ _ => {focusOn(null)}}>Escape</button>
-        </section>
-      </section>
-      )}
-      {entities && entities[focus] &&  (
-        <section className='flex gap-4 m-8'>
-          <EntityCard face={9} />
-          <EntityCard face={hero?.heroId} />
-          <EntityCard face={23} />
-        </section>
+        <main className="grid justify-center gap-4">
+          <section className='flex gap-4'>
+            <PlaceOfMob />
+            <EntityCard tw="bg-stone-700" mob={entities?.[focus]} />
+            <PlaceOfMob />
+          </section>
+
+          <section className="m-4">
+            <section className="w-96 grid grid-cols-2 gap-2 p-4">
+              <button className="rounded-lg p-2 text-lg bg-orange-400" onClick={fight}>Fight</button>
+              <button className="rounded-lg p-2 text-lg bg-orange-400" onClick={skill}>Skill</button>
+              <button className="rounded-lg p-2 text-lg bg-orange-400" onClick={talk}>Talk</button>
+              <button className="rounded-lg p-2 text-lg bg-orange-400" onClick={() => focusOn(null)}>Escape</button>
+            </section>
+          </section>
+
+          <section className='flex gap-4'>
+            <PlaceOfMob />
+            <EntityCard mob={hero} />
+            <PlaceOfMob />
+          </section>
+
+        </main>
       )}
     </section>
 
